4aafd603fcee8a1aa6a6993ad969178d8be5c220
guoli <guoli@zhihu.com>
2022-03-29 13:33:17 +0800

    Initial Commit

---
+++ leeNBA/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  leeNBA
+//
+//  Created by 郭立 on 29.3.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ leeNBA/leeNBAApp.swift
@@ -0,0 +1,17 @@
+//
+//  leeNBAApp.swift
+//  leeNBA
+//
+//  Created by 郭立 on 29.3.22.
+//
+
+import SwiftUI
+
+@main
+struct leeNBAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
