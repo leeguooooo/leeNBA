38344068a754e5c58ff4113db850acbb0b15fa2f
guoli <guoli@zhihu.com>
2022-03-29 14:42:49 +0800

    init

--- leeNBA/ContentView.swift
+++ leeNBA/ContentView.swift
@@ -9,8 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        VStack {
+            Text("Hello, Lisa.")
+                .foregroundColor(.white)
             .padding()
+            HStack {
+                Text("This is a NBA App.")
+                Text("欢迎体验。")
+                    .font(.subheadline)
+            }
+        }
     }
 }
 
